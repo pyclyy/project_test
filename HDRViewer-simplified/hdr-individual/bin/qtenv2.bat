82cd08f903e9294010e92c3032275c400a34dadc
tanpei <peiyongchao.ok@163.com>
2019-06-12 11:18:02 +0800

    HDR 查看器

---
+++ HDRViewer-simplified/hdr-individual/bin/qtenv2.bat
@@ -0,0 +1,5 @@
+echo off
+echo Setting up environment for Qt usage...
+set PATH=F:\Qt\\5.6\msvc2015_64\bin;%PATH%
+cd /D F:\Qt\\5.6\msvc2015_64
+echo Remember to call vcvarsall.bat to complete environment setup!
